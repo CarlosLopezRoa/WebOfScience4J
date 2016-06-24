4eae3135b853ee26eef7510e534d6f3d1dbf85fc
mr3m <carlos@prosciana.com>
2016-06-24 09:37:20 +0200

    iter

--- Cypher.sql
+++ Cypher.sql
@@ -40,11 +40,148 @@ WHERE s.article_id = ar.article_id
 CREATE (ar)-[r:isArticle]->(s)
 ;
 
+
 MATCH (ar:Articles)-[:isArticle]-(s:Signatures)-[:isAuthor]-(au:Authors)
+WHERE ar.article_id in range (1,1000)
 WITH (ar.article_id) as article, collect(au.author_id) as authorlist
 MATCH (au1:Authors),(au2:Authors)
 WHERE au1.author_id in authorlist and au2.author_id in authorlist
 MERGE (au1)-[r:isCoauthor]-(au2)
+;
+
+
+MATCH (a:Authors)-[r:isCoauthor]-(a:Authors)
+RETURN count(r)
+;
+
+MATCH (a:Authors)-[r:isCoauthor]-(a:Authors)
+DELETE r
+;
+
+MATCH (ar:Articles)-[:isArticle]-(s:Signatures)-[:isAuthor]-(au:Authors)
+WHERE ar.article_id in range(0,1000)
+WITH (ar.article_id) as article, collect(au.author_id) as authorlist
+MATCH (ar:Articles),(au:Authors)
+WHERE ar.article_id = article and au.author_id in authorlist
+MERGE (ar)<-[r:isWritten]-(au)
+;
+
+MATCH p = (ar1:Articles)-[r1:isWritten]-(au1:Authors)-[r2:isCoauthor*0..5]-(au2:Authors)-[r3:isWritten]-(ar2:Articles)
+WHERE ar1.article_id = 10 and ar2.article_id = 10
+RETURN (ar1.article_id) as ar1,(ar2.article_id) as ar2 ,min(length(p)) as d
+;
+
+---
+
+MATCH (n:Articles)-[r:isWritten]-(m:Authors)
+WITH m.author_id as ntop, COUNT(r) as rank 
+order by rank desc
+RETURN rank, count(ntop)
+order by rank desc
+;
+
+MATCH (n:Articles)-[r:isWritten]-(m:Authors)
+WITH m.author_id as ntop, COUNT(r) as rank 
+order by rank desc
+RETURN rank
+;
+
+MATCH (n:Authors)-[r:isCoauthor]-(m:Authors)
+WITH n.author_id as ntop, COUNT(r) as rank 
+order by rank desc
+RETURN rank, count(ntop)
+order by rank desc
+;
+
+MATCH (n:Authors)-[r:isCoauthor]-(m:Authors)
+WITH n.author_id as ntop, COUNT(r) as rank 
+order by rank desc
+RETURN rank
+;
+
+MATCH (n:Authors)-[r:isCoauthor]-(m:Authors)
+RETURN n.author_id as ntop, COUNT(r) as rank 
+order by rank desc
+limit 10
+;
+
+MATCH p = (n:Authors)-[r:isCoauthor*0..5]-(m:Authors)
+WHERE n.author_id = 271160
+WITH  m.author_id as vicinity, length(p) as d 
+order by rank desc
+limit 10
+;
+
+
+---
+
+MATCH (n:Authors)-[r:isCoauthor]-(m:Authors)
+WITH n.author_id as ntop, COUNT(r) as rank 
+order by rank desc
+RETURN avg(rank)
+;
+
+MATCH (n:Authors)-[r:isCoauthor]-(m:Authors)
+WITH n.author_id as ntop, COUNT(r) as rank 
+order by rank desc
+MATCH (ar:Articles)
+WHERE 
+RETURN n
+;
+
+
+
+RETURN collect(ntop) as autop
+MATCH p = (au1:Authors)-[r:isCoauthor*0..5]-(au2:Authors)
+WHERE au1.author_id in autop and au2.author_id in autop
+RETURN (au1.author_id),(au2.author_id),min(length(p))
+;
+
+
+
+---
+
+MATCH (n:Authors)-[r:isCoauthor]->(m:Authors)
+WITH n as au, COUNT(r) as rank 
+RETURN rank, count(au)
+order by rank asc
+;
+
+MATCH (n:Authors)-[r:isCoauthor]->(m:Authors)
+RETURN n as au, COUNT(r) as rank 
+order by rank desc
+limit 10
+;
+
+MATCH p = (s1:Signatures)-[r1:isAuthor]-(au1:Authors)-[r2:isCoauthor*]-(au2:Authors)-[r3:isAuthor]-(s2:Signatures)
+RETURN (s1.signature_id),(s2.signature_id),length (p)
+limit 100000
+;
+
+MATCH p = (s1:Signatures)-[r1:isAuthor]-(au1:Authors)-[r2:isCoauthor*]-(au2:Authors)-[r3:isAuthor]-(s2:Signatures)
+WHERE s1.signature_id = 1 and s2.signature_id = 1
+RETURN (s1.signature_id),(s2.signature_id),length (p)
+limit 10
+;
+
+MATCH (s:Signatures)
+WHERE s.signature_id = 1
+RETURN *
+;
+
+MATCH (a:Authors)-[r:isCoauthor*..0]-(b:Authors)
+WHERE a.author_id = 2
+RETURN * 
+;
+
+
+MATCH p = (au1:Authors)-[r:isCoauthor*]-(au2:Authors)
+WHERE au1.author_id in range (1000,1100) and au2.authorlist in range(1000,1100)
+RETURN (au1.author_id),(au2.author_id),length(p)
+;
+
+MATCH p = (au1:Authors)-[r:isCoauthor*]-(au2:Authors)
+RETURN max(length(p))
 ;
 
 -- EOF
